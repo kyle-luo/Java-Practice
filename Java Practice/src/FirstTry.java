52c7e8ffefd711bec7c28ca10bc88a5892275e82
kyle-luo <tsjlky@gmail.com>
2019-04-24 01:52:06 -0400

    java

---
+++ Java Practice/src/FirstTry.java
@@ -0,0 +1,8 @@
+class FirstTry
+{
+    public static void main(String[] args)
+    {
+        System.out.println("is this really working now???");
+        System.out.println("good shit");
+    }
+}
