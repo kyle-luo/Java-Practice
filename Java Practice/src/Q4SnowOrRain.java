a3af41b4708b56ebe4cbd6c3601103845d21cb1b
kyle-luo <tsjlky@gmail.com>
2019-04-24 11:28:53 -0400

    1

---
+++ Java Practice/src/Q4SnowOrRain.java
@@ -0,0 +1,26 @@
+//Assume you have access to two boolean variables, isSnowing, and isRaining, and one
+//double variable, temperature. isSnowing is true when it’s snowing and false otherwise,
+//isRaining is true when it’s raining and false otherwise, and temperature gives the outdoor
+//temperature in degrees Fahrenheit. Write code that prints “Let’s stay home.” if it’s raining,
+//snowing, or below 50 degrees Fahrenheit (10 degrees Celsius), and prints “Let’s go out!”
+//otherwise.
+
+
+public class Q4SnowOrRain
+{
+    public static void main(String[] argus)
+    {
+        boolean isSnowing = false;
+        boolean isRaining = true;
+        double temperature = 60.0;
+        if(isSnowing || isRaining || temperature < 60)
+        {
+            System.out.println("Let’s stay home.");
+        }
+        else
+        {
+            System.out.println("Let’s go out!");
+        }
+    }
+}
+
