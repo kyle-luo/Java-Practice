87eb81b03f5097cca70b1cb1031197f666ea6e0e
kyle-luo <tsjlky@gmail.com>
2019-04-27 16:00:38 -0400

    final update, passed all tests

--- Algorithms/Percolation/src/Percolation.java
+++ Algorithms/Percolation/src/Percolation.java
@@ -15,27 +15,29 @@ public class Percolation {
         grid = new boolean[n][n];
         bot = n * n + 1;
         connect = new WeightedQuickUnionUF(n * n + 2);
-//        for (int i = 1; i <= n; i++) {
-//            for (int j = 1; j <= n; j++) {
-//                grid[i - 1][j - 1] = false;
-//            }
-//        }
+        for (int row = 1; row <= n; row++) {
+            for (int col = 1; col <= n; col++) {
+                grid[row - 1][col - 1] = false;
+            }
+        }
     }
 
     public void open(int row, int col)       // open site (row, col) if it is not open already
     {
-//        if (isOpen(row, col) == false) {
+        if (!isOpen(row, col)) {
             grid[row - 1][col - 1] = true;
-//            opencount++;
-//        }
-        if (row == 1) connect.union(col, top);
-        else if (row == gridSize) connect.union(getIndex(row , col), bot);
-        else {
-            if (isOpen(row, col - 1)) connect.union(getIndex(row, col - 1), getIndex(row, col));
-            else if (isOpen(row, col + 1)) connect.union(getIndex(row, col + 1), getIndex(row, col));
-            else if (isOpen(row - 1, col)) connect.union(getIndex(row, col - 1), getIndex(row, col));
-            else if (isOpen(row + 1, col)) connect.union(getIndex(row, col + 1), getIndex(row, col));
+            opencount++;
         }
+        if (row == 1) connect.union(getIndex(row, col), top);
+        if (row == gridSize) {
+            if (isFull(row - 1, col)) connect.union(getIndex(row , col), bot);
+            if (col >1 && isFull(row, col - 1)) connect.union(getIndex(row, col - 1), getIndex(row, col));
+            if (col < gridSize && isFull(row, col + 1)) connect.union(getIndex(row, col + 1), getIndex(row, col));
+        }                                    // prevent all blocks at the bottom row be connected to top
+        if (col >1 && isOpen(row, col - 1)) connect.union(getIndex(row, col - 1), getIndex(row, col));
+        if (col < gridSize && isOpen(row, col + 1)) connect.union(getIndex(row, col + 1), getIndex(row, col));
+        if (row > 1 && isOpen(row - 1, col)) connect.union(getIndex(row - 1, col), getIndex(row, col));
+        if (row < gridSize && isOpen(row + 1, col)) connect.union(getIndex(row + 1, col), getIndex(row, col));
     }
 
     public boolean isOpen(int row, int col)  // is site (row, col) open?
@@ -59,8 +61,8 @@ public class Percolation {
         return connect.connected(top, bot);
     }
 
-    private int getIndex(int i, int j) {
-        return gridSize * (i - 1) + j;
+    private int getIndex(int row, int col) {
+        return gridSize * (row - 1) + col;
     }
 
 }
