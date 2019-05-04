e87ab13f83d472f55d53299736546d34c8cae96f
kyle-luo <tsjlky@gmail.com>
2019-05-04 11:04:25 -0400

    1

--- Algorithms/Coursera UCSD/Week 2/src/FibonacciSumLastDigit.java
+++ Algorithms/Coursera UCSD/Week 2/src/FibonacciSumLastDigit.java
@@ -19,28 +19,49 @@ public class FibonacciSumLastDigit {
 //        return sum % 10;
 //    }
 
-//    private static long getFibonacciLastDigitNaive(long n) {
-////        if (n <= 1) return n;
-////        long sum1 = 0;
-////        long sum2 = 1;
-////        for (int i = 2; i <= n; i++) {
-////            long trans = sum2;
-////            sum2 = (sum2 + sum1) % 10;
-////            sum1 = trans;
-////        }
-////        return sum2;
-////    }
+// code above is original code
 
-    private static long getFibonacciSumNaive(long n) {
-        if (n <= 1) return n;
-        long sum1 = 0;
-        long sum2 = 1;
-        for (int i = 2; i <= n + 2; i++) {
-            long trans = sum2;
-            sum2 = (sum2 + sum1) % 10;
-            sum1 = trans;
+//    private static int getFibonacciSumNaive(long n) {
+//        if (n <= 1) return (int)n;
+//        int sum1 = 0;
+//        int sum2 = 1;
+//        for (long i = 2; i <= n + 2; i++) {
+//            int trans = sum2;
+//            sum2 = (sum2 + sum1) % 10;
+//            sum1 = trans;
+//        }
+//        return Math.abs(sum2 - 1);
+//    }
+//
+//    code about is not fast enough
+
+    private static long PisanoPeriod(long num) {
+        long a = 0;
+        long b = 1;
+        long c;
+        for (long i = 0; i <= num * num; i++) {
+            c = (a + b) % num;
+            a = b;
+            b = c;
+            if (a == 0 && b == 1) return i + 1;
         }
-        return sum2 - 1;
+        return num;
+    }
+
+    private static long getFibonacciHugeNaive(long n, long m) {
+        long new_n = n % PisanoPeriod(m);
+        long a = 0;
+        long b = 1;
+        long res = new_n;
+        for (int i = 1; i < new_n; i++) {
+            res = (a + b) % m;
+            a = b;
+            b = res;
+        }
+        return res;
+    }
+    private static long getFibonacciSumNaive(long n) {
+        return getFibonacciHugeNaive(n + 2, 10) - 1;
     }
 
     public static void main(String[] args) {
