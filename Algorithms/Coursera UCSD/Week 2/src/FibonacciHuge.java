c855681b584e4fc33ae2d37305cbe311f9303c68
kyle-luo <tsjlky@gmail.com>
2019-05-04 01:12:25 -0400

    1

--- Algorithms/Coursera UCSD/Week 2/src/FibonacciHuge.java
+++ Algorithms/Coursera UCSD/Week 2/src/FibonacciHuge.java
@@ -1,53 +1,57 @@
 import java.util.*;
 
 public class FibonacciHuge {
-    private static long calc_fib(long n) {
-        if (n <= 1) return n;
-        long sum1 = 0;
-        long sum2 = 1;
-        for (int i = 2; i <= n; i++) {
-            long trans = sum2;
-            sum2 += sum1;
-            sum1 = trans;
-        }
-        return sum2;
-    }
 
-    private static long PisanoPeriod(long m) {
+    private static long PisanoPeriod(long num) {
         long a = 0;
         long b = 1;
-        long c = 0;
-        long d = 1;
-        for (int i = 0; i <= m * m; i++) {
-            long trans = b;
-            b += a;
-            a = trans;
-            c = d;
-            d = b % m;
-
+        long c;
+        for (long i = 0; i <= num * num; i++) {
+            c = (a + b) % num;
+            a = b;
+            b = c;
+            if (a == 0 && b == 1) return i + 1;
         }
+        return num;
     }
 
     private static long getFibonacciHugeNaive(long n, long m) {
-        if (n <= 1)
-            return n;
-
-        long previous = 0;
-        long current  = 1;
-
-        for (long i = 0; i < n - 1; ++i) {
-            long tmp_previous = previous;
-            previous = current;
-            current = tmp_previous + current;
+        long new_n = n % PisanoPeriod(m);
+        long a = 0;
+        long b = 1;
+        long res = 1;
+        for (int i = 1; i < new_n; i++) {
+            res = (a + b) % m;
+            a = b;
+            b = res;
         }
-
-        return current % m;
+        return res;
     }
-    
+
+//    private static long getFibonacciHugeNaive(long n, long m) {
+//        if (n <= 1)
+//            return n;
+//
+//        long previous = 0;
+//        long current  = 1;
+//
+//        for (long i = 0; i < n - 1; ++i) {
+//            long tmp_previous = previous;
+//            previous = current;
+//            current = tmp_previous + current;
+//        }
+//
+//        return current % m;
+//    }
+
+
+
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         long n = scanner.nextLong();
         long m = scanner.nextLong();
+//        System.out.println(PisanoPeriod(m));
         System.out.println(getFibonacciHugeNaive(n, m));
     }
 }
