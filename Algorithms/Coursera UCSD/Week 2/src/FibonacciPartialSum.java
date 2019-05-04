0ce035ecbfe6ce26a6cdb804e9d115607390aa01
kyle-luo <tsjlky@gmail.com>
2019-05-04 01:23:58 -0400

    1

--- Algorithms/Coursera UCSD/Week 2/src/FibonacciPartialSum.java
+++ Algorithms/Coursera UCSD/Week 2/src/FibonacciPartialSum.java
@@ -1,24 +1,26 @@
 import java.util.*;
 
 public class FibonacciPartialSum {
-    private static long getFibonacciPartialSumNaive(long from, long to) {
-        long sum = 0;
+//    private static long getFibonacciPartialSumNaive(long from, long to) {
+//        long sum = 0;
+//
+//        long current = 0;
+//        long next  = 1;
+//
+//        for (long i = 0; i <= to; ++i) {
+//            if (i >= from) {
+//                sum += current;
+//            }
+//
+//            long new_current = next;
+//            next = next + current;
+//            current = new_current;
+//        }
+//
+//        return sum % 10;
+//    }
 
-        long current = 0;
-        long next  = 1;
-
-        for (long i = 0; i <= to; ++i) {
-            if (i >= from) {
-                sum += current;
-            }
-
-            long new_current = next;
-            next = next + current;
-            current = new_current;
-        }
-
-        return sum % 10;
-    }
+    private static long getFibonacciPartialSumNaive(long from, long to)
     
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
--- Algorithms/Coursera UCSD/Week 2/src/FibonacciSumLastDigit.java
+++ Algorithms/Coursera UCSD/Week 2/src/FibonacciSumLastDigit.java
@@ -1,24 +1,44 @@
 import java.util.*;
 
 public class FibonacciSumLastDigit {
-    private static long getFibonacciSumNaive(long n) {
-        if (n <= 1)
-            return n;
+//    private static long getFibonacciSumNaive(long n) {
+//        if (n <= 1)
+//            return n;
+//
+//        long previous = 0;
+//        long current  = 1;
+//        long sum      = 1;
+//
+//        for (long i = 0; i < n - 1; ++i) {
+//            long tmp_previous = previous;
+//            previous = current;
+//            current = tmp_previous + current;
+//            sum += current;
+//        }
+//
+//        return sum % 10;
+//    }
 
-        long previous = 0;
-        long current  = 1;
-        long sum      = 1;
-
-        for (long i = 0; i < n - 1; ++i) {
-            long tmp_previous = previous;
-            previous = current;
-            current = tmp_previous + current;
-            sum += current;
+    private static long getFibonacciLastDigitNaive(long n) {
+        if (n <= 1) return n;
+        long sum1 = 0;
+        long sum2 = 1;
+        for (int i = 2; i <= n; i++) {
+            long trans = sum2;
+            sum2 = (sum2 + sum1) % 10;
+            sum1 = trans;
         }
+        return sum2;
+    }
 
+    private static long getFibonacciSumNaive(long n) {
+        long sum = 0;
+        for (long i = 0; i <= n; i++) {
+            sum += getFibonacciLastDigitNaive(i);
+        }
         return sum % 10;
     }
-    
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         long n = scanner.nextLong();
