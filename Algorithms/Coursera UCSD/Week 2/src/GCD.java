8fe4870a6a05105a5f89512a35ecc5333dd61288
kyle-luo <tsjlky@gmail.com>
2019-05-03 20:54:58 -0400

    1

--- Algorithms/Coursera UCSD/Week 2/src/GCD.java
+++ Algorithms/Coursera UCSD/Week 2/src/GCD.java
@@ -1,24 +1,38 @@
 import java.util.*;
 
 public class GCD {
-  private static int gcd_naive(int a, int b) {
-    int current_gcd = 1;
-    for(int d = 2; d <= a && d <= b; ++d) {
-      if (a % d == 0 && b % d == 0) {
-        if (d > current_gcd) {
-          current_gcd = d;
-        }
-      }
-    }
+//  private static int gcd_naive(int a, int b) {
+//    int current_gcd = 1;
+//    for(int d = 2; d <= a && d <= b; ++d) {
+//      if (a % d == 0 && b % d == 0) {
+//        if (d > current_gcd) {
+//          current_gcd = d;
+//        }
+//      }
+//    }
+//    return current_gcd;
+//  }
 
-    return current_gcd;
+  private static long gcd_naive(long a, long b) {
+    long big = Math.max(a, b);
+    long small = Math.min(a, b);
+    while (small != 0) {
+      long trans = small;
+      small = big % small;
+      big = trans;
+    }
+    return big;
   }
 
   public static void main(String args[]) {
     Scanner scanner = new Scanner(System.in);
-    int a = scanner.nextInt();
-    int b = scanner.nextInt();
+    long a = scanner.nextInt();
+    long b = scanner.nextInt();
 
     System.out.println(gcd_naive(a, b));
   }
 }
+
+//1564
+//8763
+//23
