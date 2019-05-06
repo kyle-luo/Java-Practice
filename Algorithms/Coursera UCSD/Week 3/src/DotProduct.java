6f30ae6eb5397b9fd86ccfbe4c4310490fcce97e
kyle-luo <tsjlky@gmail.com>
2019-05-06 17:34:24 -0400

    1

--- Algorithms/Coursera UCSD/Week 3/src/DotProduct.java
+++ Algorithms/Coursera UCSD/Week 3/src/DotProduct.java
@@ -1,7 +1,33 @@
 import java.util.*;
 
 public class DotProduct {
+
     private static long maxDotProduct(int[] a, int[] b) {
+        long sum = 0;
+        Arrays.sort(a);
+        Arrays.sort(b);
+        for (int i = 0; i < b.length; i++) {
+            sum += a[i] * b[i];
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = scanner.nextInt();
+        }
+        int[] b = new int[n];
+        for (int i = 0; i < n; i++) {
+            b[i] = scanner.nextInt();
+        }
+        System.out.println(maxDotProduct(a, b));
+    }
+
+    /** Below is test code
+    private static long maxDotProduct2(int[] a, int[] b) {
         //write your code here
         long result = 0;
         bubblesort(a);
@@ -25,22 +51,47 @@ public class DotProduct {
         }
     }
 
-    private static long maxDotProduct2(int[] a, int[] b) {
-
+    private static int[] gen(int length) {
+        int[] output = new int[length];
+        for (int i = 0; i < length; i++) {
+            output[i] = (int) (Math.random()* 200000 + 1 - 100000);
+        }
+        return output;
     }
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        int[] a = new int[n];
-        for (int i = 0; i < n; i++) {
-            a[i] = scanner.nextInt();
+    private static void test() {
+        while (true) {
+            int length = (int) (Math.random()* 10 + 1);
+            int[] a = gen(length);
+            System.out.println("first row  " + a);
+            int[] b = gen(length);
+            System.out.println("second row " + b);
+            if (maxDotProduct(a,b) == maxDotProduct2(a,b)) System.out.println("ok");
+            else {
+                System.out.println("Not match");
+                System.out.println(maxDotProduct(a,b));
+                System.out.println(maxDotProduct2(a,b));
+            }
         }
-        int[] b = new int[n];
-        for (int i = 0; i < n; i++) {
-            b[i] = scanner.nextInt();
-        }
-        System.out.println(maxDotProduct(a, b));
     }
+
+    private static void speedtest() {
+        while (true) {
+            int length = (int) (Math.random() * 100000 + 1);
+            int[] a = gen(length);
+            int[] b = gen(length);
+            long tStart1 = System.currentTimeMillis();
+            maxDotProduct(a, b);
+            long tEnd1 = System.currentTimeMillis();
+            maxDotProduct2(a, b);
+            long tEnd2 = System.currentTimeMillis();
+            long tDelta1 = tEnd1 - tStart1;
+            long tDelta2 = tEnd2 - tEnd1;
+
+            System.out.println("1 test " + tDelta1 / 1000);
+            System.out.println("2 test " + tDelta2 / 1000);
+        }
+    }
+     */
 }
 
