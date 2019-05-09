6f4435f8013675db8331e2ea78ea91efe9774377
kyle-luo <tsjlky@gmail.com>
2019-05-09 15:22:22 -0400

    1

--- Algorithms/Coursera UCSD/Week 3/src/CarFueling.java
+++ Algorithms/Coursera UCSD/Week 3/src/CarFueling.java
@@ -29,7 +29,7 @@ public class CarFueling {
         }
         else return -1;
     }
-
+     
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int dist = scanner.nextInt();
