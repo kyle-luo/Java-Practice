400348cd7bbca37d62bdb12220448db10ee9f89a
kyle-luo <tsjlky@gmail.com>
2019-05-14 22:57:41 -0400

    1

--- Algorithms/Coursera UCSD/Week 3/src/LargestNumber.java
+++ Algorithms/Coursera UCSD/Week 3/src/LargestNumber.java
@@ -3,8 +3,17 @@ import java.util.*;
 public class LargestNumber {
     private static String largestNumber(String[] a) {
         //write your code here
-        for 
     }
+
+//    private static String big(String a, String b){
+//        if(a.length() == b.length()) {
+//            if(Integer.valueOf(a) > Integer.valueOf(b)) return a;
+//            else return b;
+//        }
+//        else if(a.length() > b.length()) {
+//
+//        }
+//    }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
