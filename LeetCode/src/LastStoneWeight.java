cae41f4a0e50eadb99f3116351df142c7e4153ae
kyle-luo <tsjlky@gmail.com>
2019-05-21 00:25:07 -0400

    1

---
+++ LeetCode/src/LastStoneWeight.java
@@ -0,0 +1,16 @@
+public class LastStoneWeight {
+    public int lastStoneWeight(int[] stones) {
+        ArrayList<Integer> stone = new ArrayList<>();
+        for (int x: stones) {
+            stone.add(x);
+        }
+        while (stone.size() != 1) {
+            Collections.sort(stone);
+            int first = stone.remove(stone.size() - 1);
+            int second = stone.remove(stone.size() - 1);
+            stone.add(first - second);
+        }
+        return stone.get(0);
+
+    }
+}
