cfda2e1a6aa8c8b40e23fc71f3118771f0542091
kyle-luo <tsjlky@gmail.com>
2019-09-11 19:28:46 -0400

    android

---
+++ Android/Android/lib/src/main/java/com/example/lib/HelloWorld.java
@@ -0,0 +1,8 @@
+package com.example.lib;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        String output = "Hello, World";
+        System.out.println(output);
+    }
+}
---
+++ Android/Android/lib/src/main/java/com/example/lib/week3.java
@@ -0,0 +1,9 @@
+package com.example.lib;
+
+public class week3 {
+    public static void main (String[] args){
+        System.out.printf("%s\n%s\n", "Welcome to", 10.2);
+        System.out.printf("%s\n%d\n", "Welcome", 10.2);
+        System.out.printf("%s\n%f\n", "Welcome", 10.2);
+    }
+}
