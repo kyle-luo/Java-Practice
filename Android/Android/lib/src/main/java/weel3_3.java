2dd368003db7f1aee0598be829ea54818faa4191
kyle-luo <tsjlky@gmail.com>
2019-09-11 20:38:37 -0400

    Create weel3_3.java

---
+++ Android/Android/lib/src/main/java/weel3_3.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class weel3_3 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("First number: ");
+        int n1 = input.nextInt();
+        System.out.print("Second number: ");
+        int n2 = input.nextInt();
+        System.out.print("Final number: ");
+        int n3 = input.nextInt();
+        System.out.printf("The mean of 3 numbers is: %f\n", (float)(n1 + n2 +n3)/3);
+    }
+}
