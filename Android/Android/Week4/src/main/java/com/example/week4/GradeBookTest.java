812eb43dc20beff76b7d6ad0738093df716c6d98
kyle-luo <tsjlky@gmail.com>
2019-09-19 17:34:58 -0400

    Update GradeBookTest.java

--- Android/Android/Week4/src/main/java/com/example/week4/GradeBookTest.java
+++ Android/Android/Week4/src/main/java/com/example/week4/GradeBookTest.java
@@ -6,13 +6,17 @@ public class GradeBookTest {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         GradeBook gb = new GradeBook();
-        System.out.println("Please Enter Course Name:");
-        String courseName = input.nextLine();
-        gb.display(courseName);
 
         System.out.println("Enter 2 numbers: ");
         int a = input.nextInt();
         int b = input.nextInt();
         System.out.printf("The sum of two number is %d", gb.addition(a, b));
+
+
+        System.out.println("Please Enter Course Name:");
+        String courseName = input.nextLine();
+        gb.display(courseName);
+
+
     }
 }
