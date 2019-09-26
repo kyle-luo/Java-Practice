2b7692c4ac0e69515526413c3089c67535fafbbf
kyle-luo <tsjlky@gmail.com>
2019-09-25 21:32:46 -0400

    xml

--- Android/Beer advisor/app/src/main/java/com/example/beeradvisor/MainActivity.java
+++ Android/Beer advisor/app/src/main/java/com/example/beeradvisor/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.beeradvisor;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.*;
+import android.widget.*;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClickFindBeer(View view) {
+        TextView brands = (TextView) findViewById(R.id.brands);
+
+        Spinner color = (Spinner) findViewById(R.id.color);
+
+        String beerType = String.valueOf(color.getSelectedItem());
+
+        brands.setText(beerType);
     }
 }
