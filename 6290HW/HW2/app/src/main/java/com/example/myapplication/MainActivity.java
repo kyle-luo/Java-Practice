9c602425a990d7c3f3a5fbed594f9d7acc35bf85
kyle-luo <tsjlky@gmail.com>
2019-10-06 05:46:56 -0400

    calculator done

--- 6290HW/HW2/app/src/main/java/com/example/myapplication/MainActivity.java
+++ 6290HW/HW2/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -12,7 +12,9 @@ public class MainActivity extends AppCompatActivity {
     TextView display;
     Button n1, n2, n3, n4, n5, n6, n7, n8, n9, n0, multiply, divide, plus, minus, equal, clear;
     String operator = null;
-    String v1, v2, result;
+    String v1, v2;
+    Double result;
+    Boolean reset = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,7 +42,11 @@ public class MainActivity extends AppCompatActivity {
         n1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (display.getText().equals("Number") || display.getText().equals("0") || operator != null) display.setText("1");
+                if (display.getText().equals("Number") || display.getText().equals("0") || reset || result != null) {
+                    display.setText("1");
+                    reset = false;
+                    result = null;
+                }
                 else display.setText(display.getText() + "1");
             }
         });
@@ -48,7 +54,11 @@ public class MainActivity extends AppCompatActivity {
         n2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (display.getText().equals("Number") || display.getText().equals("0") || operator != null) display.setText("2");
+                if (display.getText().equals("Number") || display.getText().equals("0") || reset || result != null) {
+                        display.setText("2");
+                        reset = false;
+                        result = null;
+                }
                 else display.setText(display.getText() + "2");
             }
         });
@@ -56,7 +66,11 @@ public class MainActivity extends AppCompatActivity {
         n3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (display.getText().equals("Number") || display.getText().equals("0") || operator != null) display.setText("3");
+                if (display.getText().equals("Number") || display.getText().equals("0") || reset || result != null) {
+                    display.setText("3");
+                    reset = false;
+                    result = null;
+                }
                 else display.setText(display.getText() + "3");
             }
         });
@@ -64,7 +78,11 @@ public class MainActivity extends AppCompatActivity {
         n4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (display.getText().equals("Number") || display.getText().equals("0") || operator != null) display.setText("4");
+                if (display.getText().equals("Number") || display.getText().equals("0") || reset || result != null) {
+                    display.setText("4");
+                    reset = false;
+                    result = null;
+                }
                 else display.setText(display.getText() + "4");
             }
         });
@@ -72,7 +90,11 @@ public class MainActivity extends AppCompatActivity {
         n5.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (display.getText().equals("Number") || display.getText().equals("0") || operator != null) display.setText("5");
+                if (display.getText().equals("Number") || display.getText().equals("0") || reset || result != null) {
+                    display.setText("5");
+                    reset = false;
+                    result = null;
+                }
                 else display.setText(display.getText() + "5");
             }
         });
@@ -80,7 +102,11 @@ public class MainActivity extends AppCompatActivity {
         n6.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (display.getText().equals("Number") || display.getText().equals("0") || operator != null) display.setText("6");
+                if (display.getText().equals("Number") || display.getText().equals("0") || reset || result != null) {
+                    display.setText("6");
+                    reset = false;
+                    result = null;
+                }
                 else display.setText(display.getText() + "6");
             }
         });
@@ -88,7 +114,11 @@ public class MainActivity extends AppCompatActivity {
         n7.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (display.getText().equals("Number") || display.getText().equals("0") || operator != null) display.setText("7");
+                if (display.getText().equals("Number") || display.getText().equals("0") || reset || result != null) {
+                    display.setText("7");
+                    reset = false;
+                    result = null;
+                }
                 else display.setText(display.getText() + "7");
             }
         });
@@ -96,7 +126,11 @@ public class MainActivity extends AppCompatActivity {
         n8.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (display.getText().equals("Number") || display.getText().equals("0") || operator != null) display.setText("8");
+                if (display.getText().equals("Number") || display.getText().equals("0") || reset || result != null) {
+                    display.setText("8");
+                    reset = false;
+                    result = null;
+                }
                 else display.setText(display.getText() + "8");
             }
         });
@@ -104,7 +138,11 @@ public class MainActivity extends AppCompatActivity {
         n9.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (display.getText().equals("Number") || display.getText().equals("0") || operator != null) display.setText("9");
+                if (display.getText().equals("Number") || display.getText().equals("0") || reset || result != null) {
+                    display.setText("9");
+                    reset = false;
+                    result = null;
+                }
                 else display.setText(display.getText() + "9");
             }
         });
@@ -112,7 +150,11 @@ public class MainActivity extends AppCompatActivity {
         n0.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (display.getText().equals("Number") || display.getText().equals("0") || operator != null) display.setText("0");
+                if (display.getText().equals("Number") || display.getText().equals("0") || reset || result != null) {
+                    display.setText("0");
+                    reset = false;
+                    result = null;
+                }
                 else display.setText(display.getText() + "0");
             }
         });
@@ -122,8 +164,15 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 if (display.getText().equals("Number"));
                 else {
+                    if (v1 != null && operator != null) {
+                        v2 = display.getText().toString();
+                        result = calculate(v1, operator, v2);
+                        show(result);
+                    }
                     v1 = display.getText().toString();
                     operator = "+";
+                    reset = true;
+                    result = null;
                 }
             }
         });
@@ -133,8 +182,15 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 if (display.getText().equals("Number"));
                 else {
-                    float v1 = Float.parseFloat(display.getText().toString());
+                    if (v1 != null && operator != null) {
+                        v2 = display.getText().toString();
+                        result = calculate(v1, operator, v2);
+                        show(result);
+                    }
+                    v1 = display.getText().toString();
                     operator = "-";
+                    reset = true;
+                    result = null;
                 }
             }
         });
@@ -144,8 +200,15 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 if (display.getText().equals("Number"));
                 else {
-                    float v1 = Float.parseFloat(display.getText().toString());
+                    if (v1 != null && operator != null) {
+                        v2 = display.getText().toString();
+                        result = calculate(v1, operator, v2);
+                        show(result);
+                    }
+                    v1 = display.getText().toString();
                     operator = "*";
+                    reset = true;
+                    result = null;
                 }
             }
         });
@@ -155,8 +218,15 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 if (display.getText().equals("Number"));
                 else {
-                    float v1 = Float.parseFloat(display.getText().toString());
+                    if (v1 != null && operator != null) {
+                        v2 = display.getText().toString();
+                        result = calculate(v1, operator, v2);
+                        show(result);
+                    }
+                    v1 = display.getText().toString();
                     operator = "/";
+                    reset = true;
+                    result = null;
                 }
             }
         });
@@ -164,7 +234,15 @@ public class MainActivity extends AppCompatActivity {
         equal.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                
+                if (operator != null) {
+                    v2 = display.getText().toString();
+                    result = calculate(v1, operator, v2);
+                    show(result);
+                }
+                v1 = null;
+                v2 = null;
+                operator = null;
+                result = null;
             }
         });
 
@@ -179,6 +257,30 @@ public class MainActivity extends AppCompatActivity {
                 display.setText("Number");
             }
         });
+
+    }
+
+    private double calculate(String a, String operator, String b) {
+        if (operator == "+") {
+            return Double.parseDouble(a) + Double.parseDouble(b);
+        }
+        else if (operator == "-") {
+            return Double.parseDouble(a) - Double.parseDouble(b);
+        }
+        else if (operator == "*") {
+            return Double.parseDouble(a) * Double.parseDouble(b);
+        }
+        else {
+            return Double.parseDouble(a) / Double.parseDouble(b);
+        }
+    }
+
+    private void show(double result){
+        if (Math.round(result) == result) {
+            long resultI = Math.round(result);
+            display.setText(Long.toString(resultI));
+        }
+        else display.setText(Double.toString(result));
     }
 
 
